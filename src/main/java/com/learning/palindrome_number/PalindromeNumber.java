d0525fa468d90255b62d912e38fe588a7421ce5c
kousyoukun <cs3058672>
2019-04-19 22:28:43 +0800

    update

---
+++ src/main/java/com/learning/palindrome_number/PalindromeNumber.java
@@ -0,0 +1,45 @@
+package com.learning.palindrome_number;
+
+/**
+ * 回文数
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ *
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ */
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-121));
+    }
+
+    // 转换成字符串方法
+    private static boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        boolean flg = true;
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return flg;
+    }
+}
